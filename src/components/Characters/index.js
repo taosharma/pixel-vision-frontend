7e09939929dca1d436090d44bc97e11558456ecb
taosharma <taosharma@gmail.com>
2020-06-11 13:23:01 +0100

    restyled character page

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -3,16 +3,22 @@ import React from 'react';
 import css from './Characters.module.css';
 
 function Characters({ characters }) {
-  const { image1, image2, title1, podcast, ben, tao } = characters;
+  const { image1, image2, title, podcast, ben, tao } = characters;
   return (
     <article className={css.Characters}>
       <img className={css.image} src={image1} />
-      <h3 className={css.title}>{title1}</h3>
-      <p className={css.text}>{podcast}</p>
+      <h3 className={css.title}>{title}</h3>
+      {podcast.map((paragraph) => (
+        <p className={css.text}>{paragraph}</p>
+      ))}
       <h3 className={css.title}>Ben</h3>
-      <p className={css.text}>{ben}</p>
+      {ben.map((paragraph) => (
+        <p className={css.text}>{paragraph}</p>
+      ))}
       <h3 className={css.title}>Tao</h3>
-      <p className={css.text}>{tao}</p>
+      {tao.map((paragraph) => (
+        <p className={css.text}>{paragraph}</p>
+      ))}
       <img className={css.image} src={image2} />
     </article>
   );
