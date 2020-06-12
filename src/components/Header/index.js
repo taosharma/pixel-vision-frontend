c37c5d9b84a2ed1a76606ccbdd18e9f057bd5113
taosharma <taosharma@gmail.com>
2020-06-12 13:30:24 +0100

    added new font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,9 +14,9 @@ function Header() {
       <div>
         <h2 className={css.subtitle}>Ben and Tao's</h2>
         <div className={css.titleContainer}>
-          <h1 className={css.title}>Pixel</h1>
+          <h1 className={css.title}>Pixel Vision</h1>
+          {/* <h1 className={css.title}>Vision</h1> */}
           <img className={css.logo} src={logo} alt='Pixel Vision Logo' />
-          <h1 className={css.title}>Vision</h1>
         </div>
       </div>
       <NavigationBar className={css.NavigationBar} links={links} />
