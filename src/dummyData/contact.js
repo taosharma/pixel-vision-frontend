c1bcde6383feb1ef1cb721ef9137ce32666950ac
taosharma <taosharma@gmail.com>
2020-06-15 12:00:08 +0100

    edited contact email

--- src/dummyData/contact.js
+++ src/dummyData/contact.js
@@ -1,7 +1,7 @@
 const contact = {
   image: 'https://i.imgur.com/E4DozTQ.jpg?1',
   title: 'Send us an email at:',
-  email: 'pixelvision@gmail.com',
+  email: 'pixelvisionpodcast@gmail.com',
   text: `We're not on social media, but we'd still love to know what you think of the podcast.`,
 };
 
