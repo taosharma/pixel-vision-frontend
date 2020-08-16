a251d4070e630ab15e2cd5bb48006180859319be
unknown <taosharma@gmail.com>
2020-08-16 13:18:51 +0100

    hyperlinks working

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import Linkify from 'react-linkify';
+
 import css from './Post.module.css';
 
 import { useParams } from 'react-router-dom';
@@ -8,6 +10,10 @@ import CommentForm from '../CommentForm';
 import Comment from '../Comment';
 
 import exampleEpisodes from '../../dummyData/episodes';
+
+function createMarkup(string) {
+  return { __html: string };
+}
 
 function Post({ post, submitComment, handleCurrentPost }) {
   const [thisPost, setThisPost] = useState(exampleEpisodes[0]);
@@ -24,85 +30,92 @@ function Post({ post, submitComment, handleCurrentPost }) {
   }, [post, handleCurrentPost, id, postId]);
 
   return (
-    <main className={css.container}>
-      <section className={css.Post}>
-        <img
-          className={css.headerImage}
-          src={headerImage.image}
-          alt={headerImage.alt}
-          title={headerImage.alt}
-        />
-        <h3 className={css.title}>{text.title}</h3>
-        <p className={css.date}>{text.date}</p>
-        {text.body.map((paragraph, index) => {
-          if (index % 4 === 0 && index !== 0) {
-            return (
-              <div key={index}>
-                <img
-                  className={css.image}
-                  src={images[index].image}
-                  alt={images[index].alt}
-                  title={images[index].alt}
+    <Linkify>
+      <main className={css.container}>
+        <section className={css.Post}>
+          <img
+            className={css.headerImage}
+            src={headerImage.image}
+            alt={headerImage.alt}
+            title={headerImage.alt}
+          />
+          <h3 className={css.title}>{text.title}</h3>
+          <p className={css.date}>{text.date}</p>
+          {text.body.map((paragraph, index) => {
+            if (index % 4 === 0 && index !== 0) {
+              return (
+                <div key={index}>
+                  <img
+                    className={css.image}
+                    src={images[index].image}
+                    alt={images[index].alt}
+                    title={images[index].alt}
+                    key={index}
+                  />
+                  <p
+                    className={css.text}
+                    dangerouslySetInnerHTML={createMarkup(paragraph)}
+                  ></p>
+                </div>
+              );
+            } else
+              return (
+                <p
+                  className={css.text}
+                  dangerouslySetInnerHTML={createMarkup(paragraph)}
                   key={index}
-                />
-                <p className={css.text}>{paragraph}</p>
-              </div>
-            );
-          } else
-            return (
-              <p className={css.text} key={index}>
-                {paragraph}
-              </p>
-            );
-        })}
-        {text.contentWarning.length > 0 && (
-          <div className={css.text}>
-            <p>{text.contentWarning[0]}</p>
-          </div>
-        )}
-        {audioLink && (
-          <iframe
-            src={audioLink}
-            title={id}
-            className={css.audio}
-            frameBorder={'0'}
-          ></iframe>
-        )}
-        {text.audioExtracts.length > 0 && (
-          <div className={css.text}>
-            <p>Audio Extracts:</p>
-            <ul>
-              {text.audioExtracts.map((audioExtract, index) => (
-                <li key={index}>{audioExtract}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {text.clarifications.length > 0 && (
-          <div className={css.text}>
-            <p>Clarifications:</p>
-            <ol>
-              {text.clarifications.map((clarification, index) => (
-                <li key={index}>{clarification}</li>
-              ))}
-            </ol>
-          </div>
-        )}
-      </section>
-      {comments.length > 0 && (
-        <section className={css.commentContainer}>
-          <h3 className={css.commentTitle}>Comments:</h3>
-          {comments.map((comment, index) => (
-            <Comment comment={comment} index={index} key={index} />
-          ))}
+                ></p>
+              );
+          })}
+          {text.contentWarning.length > 0 && (
+            <div className={css.text}>
+              <p>{text.contentWarning[0]}</p>
+            </div>
+          )}
+          {audioLink && (
+            <iframe
+              src={audioLink}
+              title={id}
+              className={css.audio}
+              frameBorder={'0'}
+            ></iframe>
+          )}
+          {text.audioExtracts.length > 0 && (
+            <div className={css.text}>
+              <p>Audio Extracts:</p>
+              <ul>
+                {text.audioExtracts.map((audioExtract, index) => (
+                  <li key={index}>{audioExtract}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {text.clarifications.length > 0 && (
+            <div className={css.text}>
+              <p>Clarifications:</p>
+              <ol>
+                {text.clarifications.map((clarification, index) => (
+                  <li key={index}>{clarification}</li>
+                ))}
+              </ol>
+            </div>
+          )}
         </section>
-      )}
-      <CommentForm
-        submitComment={submitComment}
-        postId={id}
-        comments={comments}
-      />
-    </main>
+        {comments.length > 0 && (
+          <section className={css.commentContainer}>
+            <h3 className={css.commentTitle}>Comments:</h3>
+            {comments.map((comment, index) => (
+              <Comment comment={comment} index={index} key={index} />
+            ))}
+          </section>
+        )}
+        <CommentForm
+          submitComment={submitComment}
+          postId={id}
+          comments={comments}
+        />
+      </main>
+    </Linkify>
   );
 }
 
